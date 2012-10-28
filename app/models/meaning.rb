4b28ca506a2e544995392175fd128c4c20df409e
Karthikeyan A K <mindaslab@gmail.com>
2012-10-28 15:53:25 +0530

    added length validations

--- app/models/meaning.rb
+++ app/models/meaning.rb
@@ -2,7 +2,7 @@ class Meaning < ActiveRecord::Base
   belongs_to :word
   belongs_to :user
   attr_accessible :content, :word_id
-  validates :content, presence: true
+  validates :content, presence: true, length: { maximum: 500 }
 
   def vote_up user
   end
--- app/models/word.rb
+++ app/models/word.rb
@@ -2,5 +2,5 @@ class Word < ActiveRecord::Base
   has_many :meanings
   belongs_to :user
   attr_accessible :content
-  validates :content, presence: true, uniqueness: {case_sensitive: false}
+  validates :content, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 80 }
 end
